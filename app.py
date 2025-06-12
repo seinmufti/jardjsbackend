3b4d453b893bb63c6f23d0399560346a91ea4fb9
sein <hussein.mufti01@gmail..com>
2025-06-13 01:42:38 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify, send_from_directory
+from flask_cors import CORS
+from jard.jard import Jard
+
+app = Flask(__name__, static_folder="../frontend/dist", static_url_path="/")
+CORS(app)  # Allows requests from React
+
+
+@app.route("/")
+def home():
+    return send_from_directory(app.static_folder, "index.html")
+
+
+@app.route("/jard", methods=["POST"])
+def jard():
+    jard = Jard()
+
+    data = request.json
+    result = jard.slide_window(data)
+    return jsonify({"result": result})
+
+
+if __name__ == "__main__":
+    # app.run(debug=True)
+    app.run()
